4f12a122aa946e406b03cbe5eefcc7c6827f868a
Abel Morales <abel291m@gmail.com>
2024-07-22 08:31:26 -0500

    filter list tab stock-entry

--- app/Filament/Resources/StockEntryResource/Pages/ListStockEntry.php
+++ app/Filament/Resources/StockEntryResource/Pages/ListStockEntry.php
@@ -2,14 +2,30 @@
 
 namespace App\Filament\Resources\StockEntryResource\Pages;
 
+use App\Enums\StockStatuEnum;
 use App\Filament\Resources\StockEntryResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListStockEntry extends ListRecords
 {
     protected static string $resource = StockEntryResource::class;
+    public function getTabs(): array
+    {
+        $stockStatu = [];
+        foreach (StockStatuEnum::cases() as $key => $case) {
+            $stockStatu[$case->value] = Tab::make($case->getLabel())
+                ->icon($case->getIcon())
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', $case->value));
+        }
+        return [
 
+            'all' => Tab::make('Todas las entradas'),
+            ...$stockStatu
+        ];
+    }
     protected function getHeaderActions(): array
     {
         return [
